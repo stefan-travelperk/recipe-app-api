80c42a21f75fd843579296d28afbc27be3a131e6
Stefan <stefan.szabo@travelperk.com>
2020-07-24 15:14:09 +0200

    added recipe model

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -21,7 +21,7 @@ class PublicIngredientsApiTests(TestCase):
 
     def test_login_required(self):
         """test that login is required to acces the endpoint"""
-        res =self.client.get(INGREDIENTS_URL)
+        res = self.client.get(INGREDIENTS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
